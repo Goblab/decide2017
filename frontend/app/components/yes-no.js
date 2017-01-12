2908976a362cd32989247ee7e5abc175cbc0e601
Mbiondo <mbiondo@goblab.org>
2017-01-12 18:17:55 -0300

    kawaiii

--- frontend/app/components/yes-no.js
+++ frontend/app/components/yes-no.js
@@ -3,6 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	answer: null,
 
+
+	yesSelected: Ember.computed('answer', 'answer.value', function (){
+		var answer = this.get('answer');
+		if (answer && answer.get('value') == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}),
+
+	noSelected: Ember.computed('answer.value', function () {
+		var answer = this.get('answer');
+		if (answer && answer.get('value') == 2) {
+			return true;
+		} else {
+			return false;
+		}
+	}),
+
 	actions: {
 		yes: function () {
 			if (this.get('answer')) {
