25afc44eb8723665e4f31fbff6a13b834b70f6c9
Mbiondo <mbiondo@goblab.org>
2017-01-25 20:18:28 -0300

    candidates order

--- frontend/app/models/candidate.js
+++ frontend/app/models/candidate.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	order: DS.attr('number'),
 	name: DS.attr('string'),
 	facebook: DS.attr('string'),
 	twitter: DS.attr('string'),
--- frontend/app/routes/alliances/view.js
+++ frontend/app/routes/alliances/view.js
@@ -6,7 +6,14 @@ export default Ember.Route.extend({
   model: function(params) {
 	return Ember.RSVP.hash({
       alliance: this.store.find('alliance', params.alliance_id),
-      candidates: this.store.query('candidate', {alliance: params.alliance_id, sort: "position ASC"}),
+      candidates: this.store.query('candidate', {alliance: params.alliance_id, sort: "order ASC"}),
  	})  	
   },
+
+
+  setupController: function (controller, model) {
+  	this._super(controller, model);
+  	model.candidates.sortBy('order');
+  }
+  
 });
